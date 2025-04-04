03a175bdac2712353a8c36c556948f054c9b6a53
Fernando2198 <fernando_frias@ciencias.unam.mx>
2025-04-04 13:33:35 -0600

    <FEAT:vista-registroVisitante>

--- pumacheck/usuarios/urls.py
+++ pumacheck/usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import opcionesRegistroIH, registroEstudianteIH, registrosSalidasIH
+from .views import opcionesRegistroIH, registroEstudianteIH, registroVisitanteIH, registrosSalidasIH
 
 urlpatterns = [
     path('opcionesRegistro/', opcionesRegistroIH, name='opcionesRegistro'),
     path('registroEstudiante/', registroEstudianteIH, name='registroEstudiante'),
     path('registrosSalidas/', registrosSalidasIH, name='registrosSalidas'),
+    path('registroVisitante/', registroVisitanteIH, name='registroVisitante'),
 ]
--- pumacheck/usuarios/views.py
+++ pumacheck/usuarios/views.py
@@ -9,4 +9,7 @@ def registroEstudianteIH(request):
 def registrosSalidasIH(request):
     return render(request, 'usuarios/registrosSalidas.html')
 
+def registroVisitanteIH(request):
+    return render(request, 'usuarios/registroVisitante.html')
+
 # Create your views here.
