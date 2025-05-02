09593a2fa148c040415f036dd99e6c1807d6a904
César <c10.0@ciencias.unam.mx>
2025-05-01 18:41:10 -0600

    Actulización

--- pumacheck/usuarios/models.py
+++ pumacheck/usuarios/models.py
@@ -1,8 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-""" Modelo de ejemplo.
+#Modelo Visita que puede ser estudiante o visitante
+class Visita(models.Model):
+    nombre = models.CharField(max_length=50)
+    identificador = models.CharField(max_length=15)
+    tipo = models.CharField(max_length=10)
+    horaEntrada = models.DateTimeField()
+    horaSalida = models.DateTimeField(null=True, blank=True)
+
+#Modelo Vehiculo que se relaciona con Visita
+class Vehiculo(models.Model):
+    numPlaca = models.CharField(max_length=10)
+    visita = models.ForeignKey(Visita, on_delete=models.CASCADE)
+    horaEntrada = models.DateTimeField()
+    horaSalida = models.DateTimeField(null=True,blank=True)
+        
+
+#Modelo Equipo que se relaciona con Visita
+class Equipo(models.Model):
+    descripcion = models.CharField(max_length=100)
+    horaEntrada = models.DateTimeField()
+    horaSalida = models.DateTimeField(null=True,blank=True)
+    visita = models.ForeignKey(Visita, on_delete=models.CASCADE)
+        
+
+#Modelo Campus que se relaciona con Trabajador
+class Campus(models.Model):
+    nombreCampus = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=80)
+    director = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=10)
+    correo = models.EmailField(unique=True)
+
+ #Modelo de ejemplo.
 class Entrada(models.Model):
-    
     nombre = models.CharField(max_length=100)
     identificacion = models.TextField(max_length=10)
     placa = models.TextField(max_length=8)
@@ -13,10 +45,3 @@ class Entrada(models.Model):
     #def __str__(self):
     #    return 'Entrada: ' + self.nombre + ' identificación ' + self.identificacion +' placa: ' + self.placa + ' Entrada: ' + self.fechaEntrada.strftime("%H/%M") +' Salida: ' + self.fechaSalida.strftime("%H/%M")+ ' id: ' + self.aidi
     
-#Modelo Campus que se relaciona con Trabajador
-class Campus(models.Model):
-    nombreCampus = models.CharField(max_length=30)
-    direccion = models.CharField()
-    director = models.CharField(max_length=30)
-    telefono = models.CharField(max_length=10)
-    correo = models.EmailField(unique=True)"""
--- pumacheck/usuarios/tests.py
+++ pumacheck/usuarios/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
-
+import pytest
+from django.urls import path
+from datetime import date
+from .models import Entrada,Campus
 # Create your tests here.
+@pytest.mark.django_db
+def test_creacion_campus():
+    campus = Campus.objects.create(
+        nombreCampus="CU",
+        direccion="Av. Universidad 3000",
+        director="Dra. María Pérez",
+        telefono="5555555555",
+        correo="cu@unam.mx"
+    )
+    assert campus.nombreCampus == "CU"
+    assert campus.id is not None
+    
+@pytest.mark.django_db
+def create_test_data():
+    """
+    Crea una información de un  campus .
+    """
+    ##publisher = Publisher.objects.create(name="Test Publisher")
+    prueba = Campus.objects.create(
+        nombreCampus2 = "Facultad de estudios superiores Zaragoza",
+        direccion = "Enrique segobiano",
+        director = "Ronaldo Cisneros Huerta",
+        telefono = "1234567890",
+        correo = "pumitapuma@ciencias.unam.mx",
+    )
+    return prueba
