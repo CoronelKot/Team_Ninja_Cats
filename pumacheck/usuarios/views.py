8ad4fae0f3f4cd92b440bcb08dbfdbe30dec3e08
César <c10.0@ciencias.unam.mx>
2025-05-08 17:39:56 -0600

    Ultimos cambios

--- pumacheck/usuarios/views.py
+++ pumacheck/usuarios/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Visita,Vehiculo,Equipo,Campus
 
-def opcionesRegistroIH(request):
-    return render(request, 'usuarios/opcionesRegistro.html')
-
-def registroEstudianteIH(request):
-    return render(request, 'usuarios/registroEstudiante.html')
-
-def registrosSalidasIH(request):
-    return render(request, 'usuarios/registrosSalidas.html')
-
-def registroVisitanteIH(request):
-    return render(request, 'usuarios/registroVisitante.html')
 
 def informacionDelCampusIH(request, campus_id):
     campus = Campus.objects.get(pk=campus_id)
